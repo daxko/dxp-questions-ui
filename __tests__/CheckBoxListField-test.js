f6f0cd35649fae4ede69189fa90142edb726443f
Wally Shirey <waltshirey@gmail.com>
2017-10-25 15:42:35 -0500

    OPS-21028 remove failing tests

--- __tests__/CheckBoxListField-test.js
+++ __tests__/CheckBoxListField-test.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import {configure, shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-15'
-import CheckboxListField from '../src/fields/CheckboxListField'
+import CheckBoxListField from '../src/fields/CheckBoxListField'
 const json = require('../snippets/json/checkbox_question.json')
 
 configure({adapter: new Adapter() })
 
-describe('CheckboxListField component', () => {
+describe('CheckBoxListField component', () => {
   it('should render a question', () => {
-    const wrapper = shallow(<CheckboxListField question={json} />)
+    const wrapper = shallow(<CheckBoxListField question={json} />)
     expect(wrapper).toMatchSnapshot()
   })
 })
