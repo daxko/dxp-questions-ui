d173d76020964e6514a2a6c840427287b55df4e7
Griffin Pair <Griffin.Pair@daxko.com>
2019-10-31 13:18:10 -0500

    OPS-23015: Add Allow SMS checkboc to In-House Join UI

--- src/fields/CheckBoxListField.js
+++ src/fields/CheckBoxListField.js
@@ -21,6 +21,17 @@ var CheckBoxListField = React.createClass({
 		this.props.onBlur();
 	},
 
+	onChange_allow_sms: function(field, event) {
+		var new_values = this.props.answer;
+		if (event.target.checked){
+			new_values[field] = 'True';
+		}
+		else{
+			new_values[field] = 'False';
+		}
+		this.props.onChange(new_values);
+	},
+
 	render: function() {
 
 		var question = this.props.question;
@@ -30,26 +41,45 @@ var CheckBoxListField = React.createClass({
 			checked[answer[i]] = true;
 		}
 
-		return (
-			<div className="dxp-checkbox">
-				{ 
-					question.possible_answers.map(function(pa) {
-						return (
-							<label key={pa.id}>
-								<input type="checkbox" 
-									name={this.props.question_id} 
-									value={pa.id}
-									disabled={this.props.question.read_only}
-									onChange={this.onChange}
-									onBlur={this.props.onBlur}
-									checked={checked[pa.id] || false} />
-								{pa.display_value}
-							</label>
-						);
-					}.bind(this))
-				}
-			</div>
-		);
+		if(question.show_allow_sms) {
+			return (
+				<div>
+					<br />
+					<label className="dxp-phone-allow-sms-label"> 
+						<input
+						type="checkbox"
+						className="dxp-phone-allow-sms-checkbox"
+						checked={answer.allow_sms == 'True'}
+						onChange={this.onChange_allow_sms.bind(this, 'allow_sms')} />
+						{question.allow_sms_text}
+						<br/>
+						<a href={question.allow_sms_link_url} target="_blank">Learn more about SMS terms.</a>
+					</label>
+				</div>
+			)
+		}
+		else {
+			return (
+				<div className="dxp-checkbox">
+					{ 
+						question.possible_answers.map(function(pa) {
+							return (
+								<label key={pa.id}>
+									<input type="checkbox" 
+										name={this.props.question_id} 
+										value={pa.id}
+										disabled={this.props.question.read_only}
+										onChange={this.onChange}
+										onBlur={this.props.onBlur}
+										checked={checked[pa.id] || false} />
+									{pa.display_value}
+								</label>
+							);
+						}.bind(this))
+					}
+				</div>
+			);	
+		}
 	}
 
 });
--- src/form.js
+++ src/form.js
@@ -4,6 +4,7 @@ var classes = require('classnames');
 var field_factory = require('./fields/field_factory');
 var validator_factory = require('./validators/validator_factory');
 var FieldError = require('./fields/FieldError');
+var CheckBoxListField = require('./fields/CheckBoxListField');
 
 var Form = React.createClass({
   propTypes: {
@@ -253,6 +254,7 @@ var Form = React.createClass({
             />
           )}
           {show_validation && <FieldError errors={errors} />}
+          {question.show_allow_sms && <CheckBoxListField question={question} answer={this.state.answers[key]} onChange={this.onChange.bind(this, key)} />}
         </div>
       </div>
     );
